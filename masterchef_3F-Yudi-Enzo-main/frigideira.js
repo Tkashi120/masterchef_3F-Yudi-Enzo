99a55532a88b4ca19c331cae86f2f6fda8945476
Takashi <160123322+Tkashi120@users.noreply.github.com>
2025-04-25 10:53:33 -0300

    Enzo Takashi & Rômulo Yudi

--- masterchef_3F-Yudi-Enzo-main/frigideira.js
+++ masterchef_3F-Yudi-Enzo-main/frigideira.js
@@ -1,1 +1,9 @@
-let
+let randomNumber =
+Math.floor(Math.random() *10 ) + 1;
+let attempts = 0;
+
+function checkGuess(){
+    const userGuess = parseInt(document.getElementById('guess').ariaValueMax)
+    attempts 
+
+}
